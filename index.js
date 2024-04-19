a4d883e9edce60a371789ce95cdb844abb53648c
Carlston325 <130921730+Carlston325@users.noreply.github.com>
2024-04-19 14:35:46 +0100

    Posting Blogs DONE

--- index.js
+++ index.js
@@ -1,132 +1,125 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import axios from "axios";
 
 const app = express();
 const port = 3000;
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-var title = [];
-var topic = [];
-var descrip = [];
-var author = [];
-var body = [];
-var date = [];
-
-function PostedBlog(title, topic, descrip, author, body, date) {
-  this.title = title;
-  this.topic = topic;
-  this.descrip = descrip;
-  this.author = author;
-  this.body = body;
-  this.date = date;
-}
-
-function blogDetails(req, res, next) {
-  title = req.body["title"];
-  topic = req.body["topic"];
-  descrip = req.body["description"];
-  author = req.body["author"];
-  body = req.body["body"];
-  date = req.body["date"];
-
-  next();
-}
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(blogDetails);
+
+var isSubmitted = false;
+function submitted() {
+  setTimeout(() => {
+    isSubmitted = true;
+  }, 1500);
+}
 
 // Main Get Links
-app.get("/", (req, res) => {
-  res.render("index.ejs", { __dirname, title, date, body, descrip });
+app.get("/", async (req, res) => {
+  const response = await axios.get("http://localhost:3500/posts");
+  const result = response.data;
+  const recentPost = result[result.length - 1];
+
+  res.render("index.ejs", {
+    recentPost,
+    data: result,
+  });
 });
 app.get("/contact", (req, res) => {
-  res.render("contact.ejs", { __dirname });
+  res.render("contact.ejs", {});
 });
 app.get("/about", (req, res) => {
-  res.render("about.ejs", { __dirname });
+  res.render("about.ejs", {});
 });
 app.get("/blog", (req, res) => {
-  res.render("blog.ejs", { __dirname });
+  res.render("blog.ejs", {});
 });
 
-// Post request BLOGS
-app.post("/submit", (req, res) => {
-  var submitted = "Success";
+app.delete("/", (req, res) => {
+  console.log(req.body);
+});
 
-  var postedBlog = new PostedBlog(title, topic, descrip, author, body, date);
-  posts.push(postedBlog);
-  console.log(posts);
+// BLOG Post request
+app.post("/submit", async (req, res) => {
+  submitted();
+  try {
+    axios.post("http://localhost:3500/posts", {
+      title: req.body.title,
+      topic: req.body.topic,
+      descrip: req.body.description,
+      author: req.body.author,
+      body: req.body.body,
+      date: req.body.date,
+    });
 
-  res.render(
-    "index.ejs",
-    (res.locals = {
-      __dirname,
-      submitted,
-      title,
-      topic,
-      descrip,
-      author,
-      body,
-      date,
-    })
-  );
+    const response = await axios.get("http://localhost:3500/posts");
+    const result = response.data;
+    const recentPost = result[result.length - 1];
+
+    res.render("index.ejs", {
+      recentPost,
+      data: result,
+      isSubmitted,
+    });
+  } catch (err) {
+    console.log(`Failed to make request: ${err.message}`);
+    submitted = err.message;
+    res.render("index.ejs", { submitted });
+  }
 });
 
 // Topic Get Links
 app.get("/art", (req, res) => {
-  res.render("pages/art.ejs", { __dirname });
+  res.render("pages/art.ejs", {});
 });
 app.get("/business", (req, res) => {
-  res.render("pages/business.ejs", { __dirname });
+  res.render("pages/business.ejs", {});
 });
 app.get("/culture", (req, res) => {
-  res.render("pages/culture.ejs", { __dirname });
+  res.render("pages/culture.ejs", {});
 });
 app.get("/education", (req, res) => {
-  res.render("pages/education.ejs", { __dirname });
+  res.render("pages/education.ejs", {});
 });
 app.get("/family", (req, res) => {
-  res.render("pages/family.ejs", { __dirname });
+  res.render("pages/family.ejs", {});
 });
 app.get("/finance", (req, res) => {
-  res.render("pages/finance.ejs", { __dirname });
+  res.render("pages/finance.ejs", {});
 });
 app.get("/food", (req, res) => {
-  res.render("pages/food.ejs", { __dirname });
+  res.render("pages/food.ejs", {});
 });
 app.get("/games", (req, res) => {
-  res.render("pages/games.ejs", { __dirname });
+  res.render("pages/games.ejs", {});
 });
 app.get("/health", (req, res) => {
-  res.render("pages/health.ejs", { __dirname });
+  res.render("pages/health.ejs", {});
 });
 app.get("/media", (req, res) => {
-  res.render("pages/media.ejs", { __dirname });
+  res.render("pages/media.ejs", {});
 });
 app.get("/nature", (req, res) => {
-  res.render("pages/nature.ejs", { __dirname });
+  res.render("pages/nature.ejs", {});
 });
 app.get("/pets", (req, res) => {
-  res.render("pages/pets.ejs", { __dirname });
+  res.render("pages/pets.ejs", {});
 });
 app.get("/relationship", (req, res) => {
-  res.render("pages/relationship.ejs", { __dirname });
+  res.render("pages/relationship.ejs", {});
 });
 app.get("/religion", (req, res) => {
-  res.render("pages/religion.ejs", { __dirname });
+  res.render("pages/religion.ejs", {});
 });
 app.get("/sports", (req, res) => {
-  res.render("pages/sports.ejs", { __dirname });
+  res.render("pages/sports.ejs", {});
 });
 app.get("/technology", (req, res) => {
-  res.render("pages/technology.ejs", { __dirname });
+  res.render("pages/technology.ejs", {});
 });
 app.get("/world", (req, res) => {
-  res.render("pages/world.ejs", { __dirname });
+  res.render("pages/world.ejs", {});
 });
 
 // LISTEN
